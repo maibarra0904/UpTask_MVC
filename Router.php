5092b128a6896894b0576dc79cc884c927126f75
maibarra0904 <mario.ibarra.86@gmail.com>
2023-05-24 05:43:12 -0500

    240523

--- Router.php
+++ Router.php
@@ -20,7 +20,7 @@ class Router
     public function comprobarRutas()
     {
 
-        $currentUrl = $_SERVER['REQUEST_URI'] === '/' ? '/' : $_SERVER['REQUEST_URI'];
+        $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
         $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === 'GET') {
